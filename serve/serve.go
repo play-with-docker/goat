756bf24ff92847f8fbf4dafb9ad6950d062d7ded
Jonathan Leibiusky @xetorthio <ionathan@gmail.com>
2017-07-20 16:57:40 -0300

    Refactor

--- serve/serve.go
+++ serve/serve.go
@@ -6,27 +6,41 @@ import (
 	"io"
 	"log"
 	"net"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/hashicorp/yamux"
 )
 
-type bindPort struct {
-	Protocol string
-	Port     int
+type tcpTracker struct {
+	listener net.Listener
 }
 
-var boundedInterfaces map[string]bool
+type udpTracker struct {
+	listener *net.UDPConn
+	conns    map[string]net.Conn
+}
 
-var tunnel *yamux.Session
+type vxlanTracker struct {
+	listener *net.UDPConn
+	conn     net.Conn
+	stream   net.Conn
+}
 
-var udpStreams map[string]net.Conn
+type tunnel struct {
+	tcp2377 *tcpTracker
+	tcp7946 *tcpTracker
+	udp7946 *udpTracker
+	udp4789 *vxlanTracker
+}
+
+var boundedInterfaces map[string]*tunnel
+
+var client *yamux.Session
+var localIP string
 
 func main() {
-	boundedInterfaces = map[string]bool{}
-	udpStreams = map[string]net.Conn{}
+	boundedInterfaces = map[string]*tunnel{}
 
 	flag.Parse()
 
@@ -36,26 +50,11 @@ func main() {
 		log.Fatal("Please specify host:port to bind to")
 	}
 
+	localIP = strings.Split(laddr, ":")[0]
+
 	netAddr := flag.Arg(1)
 	if netAddr == "" {
 		log.Fatal("Missing network address")
-	}
-
-	ports := flag.Arg(2)
-	if ports == "" {
-		log.Fatal("Missing ports to bind to")
-	}
-
-	pts := strings.Split(ports, ",")
-	bindPorts := make([]bindPort, len(pts))
-	for i, p := range pts {
-		chunks := strings.Split(p, ":")
-		bindPorts[i].Protocol = chunks[0]
-		port, err := strconv.Atoi(chunks[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		bindPorts[i].Port = port
 	}
 
 	_, ipnet, err := net.ParseCIDR(netAddr)
@@ -65,18 +64,18 @@ func main() {
 	go func() {
 		c := time.Tick(time.Second)
 		for range c {
-			bind(ipnet, bindPorts)
+			bind(ipnet)
 		}
 	}()
 	go func() {
 		c := time.Tick(time.Second)
 		for range c {
-			if tunnel != nil {
-				_, err := tunnel.Ping()
+			if client != nil {
+				_, err := client.Ping()
 				if err != nil {
-					tunnel.Close()
-					log.Printf("Client disconnected %s\n", tunnel.RemoteAddr().String())
-					tunnel = nil
+					client.Close()
+					log.Printf("Client disconnected %s\n", client.RemoteAddr().String())
+					client = nil
 				}
 			}
 		}
@@ -96,7 +95,7 @@ func main() {
 	}
 }
 
-func bind(ipnet *net.IPNet, ports []bindPort) {
+func bind(ipnet *net.IPNet) {
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		log.Println(err)
@@ -119,75 +118,201 @@ func bind(ipnet *net.IPNet, ports []bindPort) {
 
 			if ipnet.Contains(ifaceIP) {
 				if _, found := boundedInterfaces[ip]; !found {
-					log.Printf("Found interface with IP [%s] which is contained in the given net [%s]. Binding ports: %v\n", ip, ipnet, ports)
-					for _, port := range ports {
-						if port.Protocol == "tcp" {
-							l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port.Port))
-							if err != nil {
-								log.Println(err)
-								return
-							}
-							go func(ip string, port int) {
-								for {
-									c, err := l.Accept()
-									if err != nil {
-										log.Println(err)
-										continue
-									}
-									go tunnelTCP("tcp", ip, port, c)
-								}
-							}(ip, port.Port)
-						} else {
-							// it is UDP
-							serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", ip, port.Port))
-							if err != nil {
-								log.Println(err)
-								continue
-							}
-							go func(ip string, port int) {
-								udpConn, err := net.ListenUDP("udp", serverAddr)
-								if err != nil {
-									log.Println(err)
-									return
-								}
-								buf := make([]byte, 1600)
-								for {
-									n, src, err := udpConn.ReadFromUDP(buf)
-									if err != nil {
-										log.Println(err)
-										continue
-									}
-									packet := buf[:n]
-									go tunnelUDP(ip, port, packet, src, udpConn)
-								}
-							}(ip, port.Port)
-						}
+					log.Printf("Found interface with IP [%s] which is contained in the given net [%s]. Binding ports.\n", ip, ipnet)
+					tu, err := openTunnel(ip)
+					if err != nil {
+						log.Println(err)
+						continue
 					}
-					boundedInterfaces[ip] = true
+					boundedInterfaces[ip] = tu
 				}
 			}
 		}
 	}
 }
 
-func tunnelUDP(ip string, port int, packet []byte, src *net.UDPAddr, udpConn *net.UDPConn) {
-	if tunnel == nil {
-		log.Printf("No tunnel has been made")
+func openTunnel(iface string) (*tunnel, error) {
+	tunn := &tunnel{}
+
+	t, err := openTcpTunnel(iface, 2377)
+	if err != nil {
+		return nil, err
+	}
+	tunn.tcp2377 = t
+
+	t, err = openTcpTunnel(iface, 7946)
+	if err != nil {
+		return nil, err
+	}
+	tunn.tcp7946 = t
+
+	u, err := openUdpTunnel(iface, 7946)
+	if err != nil {
+		return nil, err
+	}
+	tunn.udp7946 = u
+
+	v, err := openVXLANTunnel(iface)
+	if err != nil {
+		return nil, err
+	}
+	tunn.udp4789 = v
+
+	return tunn, nil
+}
+
+func openTcpTunnel(iface string, port int) (*tcpTracker, error) {
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", iface, port))
+	if err != nil {
+		return nil, err
+	}
+
+	tracker := &tcpTracker{listener: l}
+
+	go func() {
+		for {
+			c, err := tracker.listener.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			go tunnelTCP(iface, port, c)
+		}
+	}()
+
+	return tracker, nil
+}
+
+func sendStreamHeader(stream net.Conn, protocol, ip string, port int) {
+	header := fmt.Sprintf("%s %s %d", protocol, ip, port)
+	if len(header) < 25 {
+		header = fmt.Sprintf("%s%s", header, strings.Repeat(" ", 25-len(header)))
+	}
+	header = fmt.Sprintf("%s\n", header)
+	stream.Write([]byte(header))
+}
+
+func openVXLANTunnel(iface string) (*vxlanTracker, error) {
+	serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:4789", iface))
+	if err != nil {
+		return nil, err
+	}
+
+	l, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	c, err := net.Dial("udp", fmt.Sprintf("%s:4789", localIP))
+	if err != nil {
+		return nil, err
+	}
+
+	tracker := &vxlanTracker{listener: l, conn: c}
+	if client != nil {
+		stream, err := client.Open()
+		if err != nil {
+			log.Println("Error opening stream to tunnel VXLAN.")
+			return nil, err
+		}
+		tracker.stream = stream
+		sendStreamHeader(stream, "udp", iface, 4789)
+		go func() {
+			defer func() {
+				tracker.stream.Close()
+				tracker.stream = nil
+				tracker.conn.Close()
+				tracker.conn = nil
+			}()
+
+			io.Copy(tracker.conn, tracker.stream)
+		}()
+	}
+
+	go func() {
+		buf := make([]byte, 1600)
+		for {
+			n, _, err := tracker.listener.ReadFromUDP(buf)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if tracker.stream == nil && client != nil {
+				stream, err := client.Open()
+				if err != nil {
+					log.Println("Error opening stream to tunnel VXLAN.")
+					continue
+				}
+				tracker.stream = stream
+				sendStreamHeader(stream, "udp", iface, 4789)
+				go func() {
+					defer func() {
+						tracker.stream.Close()
+						tracker.stream = nil
+						tracker.conn.Close()
+						tracker.conn = nil
+					}()
+
+					io.Copy(tracker.conn, tracker.stream)
+				}()
+			}
+			go func(packet []byte) {
+				_, err := tracker.stream.Write(packet)
+				if err != nil {
+					tracker.stream.Close()
+					tracker.stream = nil
+				}
+			}(buf[:n])
+		}
+	}()
+
+	return tracker, nil
+}
+
+func openUdpTunnel(iface string, port int) (*udpTracker, error) {
+	serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", iface, port))
+	if err != nil {
+		return nil, err
+	}
+
+	l, err := net.ListenUDP("udp", serverAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	tracker := &udpTracker{listener: l, conns: make(map[string]net.Conn)}
+
+	go func() {
+		buf := make([]byte, 1600)
+		for {
+			n, src, err := tracker.listener.ReadFromUDP(buf)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			packet := buf[:n]
+			go tunnelUDP(iface, port, packet, src, tracker)
+		}
+	}()
+
+	return tracker, nil
+}
+
+func tunnelUDP(ip string, port int, packet []byte, src *net.UDPAddr, tracker *udpTracker) {
+	if client == nil {
+		log.Printf("No client connected.")
 		return
 	}
 
-	if port == 4789 {
-		// since it is vxlan we need to respond to the vxlan default port
-		src.Port = 4789
-	}
-	if _, found := udpStreams[src.String()]; !found {
+	if _, found := tracker.conns[src.String()]; !found {
 		var stream net.Conn
 		var err error
 		for i := 0; i < 10; i++ {
-			if tunnel == nil {
+			if client == nil {
 				break
 			}
-			stream, err = tunnel.Open()
+			stream, err = client.Open()
 			if err != nil {
 				log.Println("Error opening stream to tunnel UDP. Got: ", err)
 				time.Sleep(100 * time.Millisecond)
@@ -203,7 +328,7 @@ func tunnelUDP(ip string, port int, packet []byte, src *net.UDPAddr, udpConn *ne
 		go func(src *net.UDPAddr) {
 			defer func() {
 				stream.Close()
-				delete(udpStreams, src.String())
+				delete(tracker.conns, src.String())
 			}()
 
 			buf := make([]byte, 1600)
@@ -213,7 +338,7 @@ func tunnelUDP(ip string, port int, packet []byte, src *net.UDPAddr, udpConn *ne
 					return
 				}
 				b := buf[:n]
-				_, err = udpConn.WriteToUDP(b, src)
+				_, err = tracker.listener.WriteToUDP(b, src)
 				if err != nil {
 					return
 				}
@@ -227,45 +352,47 @@ func tunnelUDP(ip string, port int, packet []byte, src *net.UDPAddr, udpConn *ne
 		header = fmt.Sprintf("%s\n", header)
 		stream.Write([]byte(header))
 
-		udpStreams[src.String()] = stream
+		tracker.conns[src.String()] = stream
 	}
 
-	stream := udpStreams[src.String()]
+	stream := tracker.conns[src.String()]
 	_, err := stream.Write(packet)
 	if err != nil {
 		log.Println("Error while writing to stream a UDP packet. Got: ", err)
+		stream.Close()
+		delete(tracker.conns, src.String())
 	}
 }
 
-func tunnelTCP(protocol, ip string, port int, c net.Conn) {
+func tunnelTCP(ip string, port int, c net.Conn) {
 	defer c.Close()
 
-	if tunnel == nil {
-		log.Printf("No tunnel has been made")
+	if client == nil {
+		log.Printf("No client connected.")
 		return
 	}
 
 	var stream net.Conn
 	var err error
 	for i := 0; i < 10; i++ {
-		if tunnel == nil {
+		if client == nil {
 			break
 		}
-		stream, err = tunnel.Open()
+		stream, err = client.Open()
 		if err != nil {
-			log.Println("Error opening stream to tunnel UDP. Got: ", err)
+			log.Println("Error opening stream to tunnel TCP. Got: ", err)
 			time.Sleep(100 * time.Millisecond)
 			continue
 		}
 		break
 	}
 	if stream == nil {
-		log.Println("Could not open stream to tunnel UDP. Giving up.")
+		log.Println("Could not open stream to tunnel TCP. Giving up.")
 		return
 	}
 	defer stream.Close()
 
-	header := fmt.Sprintf("%s %s %d", protocol, ip, port)
+	header := fmt.Sprintf("%s %s %d", "tcp", ip, port)
 	if len(header) < 25 {
 		header = fmt.Sprintf("%s%s", header, strings.Repeat(" ", 25-len(header)))
 	}
@@ -287,15 +414,15 @@ func tunnelTCP(protocol, ip string, port int, c net.Conn) {
 }
 
 func registerTunnel(c net.Conn) {
-	if tunnel != nil {
-		log.Printf("There is already an opened tunnel. Disconnecting client %s.\n", tunnel.RemoteAddr().String())
-		tunnel.Close()
+	if client != nil {
+		log.Printf("There is already a client connected. Disconnecting %s.\n", client.RemoteAddr().String())
+		client.Close()
 	}
 	session, err := yamux.Client(c, nil)
 	if err != nil {
 		log.Println("Could not create yamux session. Got: ", err)
 		return
 	}
-	tunnel = session
+	client = session
 	log.Printf("Client connected from: %s", c.RemoteAddr().String())
 }
